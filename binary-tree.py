11c3fb2cfc82a5b0acf0adb76ddb1a7b2a4dd141
EmmanuelU <emmanuelurias60@icloud.com>
2023-04-04 14:23:20 -0700

    added more methods to my binary tree

--- binary-tree.py
+++ binary-tree.py
@@ -50,6 +50,83 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def pre_order_traversal(self):
+        elements = []
+
+        # base node
+        elements.append(self.data)
+
+        if self.left:  # visit left tree
+            elements += self.left.in_order_traversal()
+
+        # visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+    def post_order_traversal(self):
+        elements = []
+
+        if self.left:  # visit left tree
+            elements += self.left.in_order_traversal()
+
+        # visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        # base node
+        elements.append(self.data)
+
+        return elements
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        else:
+            self.right.find_max()
+
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        else:
+            self.left.find_min()
+
+    def insert(self, val):
+        if val == self.data:
+            return
+        elif val < self.data:
+            if self.left:
+                self.left.insert(val)
+            else:
+                self.left = BinarySearchTreeNode(val)
+        else:
+            if self.right:
+                self.right.insert(val)
+            else:
+                self.right = BinarySearchTreeNode(val)
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.right
+
+            min_val = self.left.find_min()
+            self.data = min_val
+            self.left = self.left.delete(min_val)
+
+        return self
+
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
@@ -63,4 +140,5 @@ def build_tree(elements):
 if __name__ == '__main__':
     numbers = [17, 4, 1, 20, 9, 23, 18, 34]
     numbers_tree = build_tree(numbers)
-    print(numbers_tree.search(20))
+    numbers_tree.insert(19)
+    print(numbers_tree.in_order_traversal())
